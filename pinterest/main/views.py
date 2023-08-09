57cdb79eab50e73dd95f444143ba29b955ae3e0e
Merih Can Çelenk <merihcancelenk@gmail.com>
2023-08-09 23:33:49 +0300

    Register ve login
    register ve login fonksiyonu üzerinde çalışıldı

--- pinterest/main/views.py
+++ pinterest/main/views.py
@@ -6,19 +6,36 @@ from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def index(request):
+    # register
     if request.method == 'POST':
+        kullanici = request.POST['kullanici']
         email = request.POST['email']
-        sifre = request.POST['sifre']
-
-        user = authenticate(request, email = email, password = sifre)
-
-        if user is not None:
-            login(request, user)
-            messages.success(request,'Giriş Yapıldı')
-            return redirect('login')
+        sifre1 = request.POST['sifre1']
+        sifre2 = request.POST['sifre2']
+        if sifre1 == sifre2:
+            if User.objects.filter(username = kullanici).exists():
+                messages.error(request, 'Kullanıcı Adı Kullanımda !')
+            elif User.objects.filter(email = email).exists():
+                messages.error(request,'Email Kullanımda !')
+            elif len(sifre1) < 6:
+                messages.error(request, 'Şifre En Az 6 Karakter Olmalıdır !')
+            elif kullanici.lower() in sifre1.lower():
+                messages.error(request, 'Kullanıcı Adı ve Şifre Aynı Olmamalıdır')
+            else:
+                user = User.objects.create_user(
+                    username = kullanici,
+                    email = email,
+                    password = sifre1
+                )
+                user.save()
+                messages.success(request,'Kayıt tamamlandı. Giriş Yapabilirsiniz.')
+                    
         else:
-            messages.error(request,'Kullanıcı Adı veya Şifre Hatalı')
-            return redirect('index')
+            messages.error(request,'Şifreler Eşleşmiyor !')
+# Kullanıcı Girişi Fonksiyonu   
+
+
+            
     return render (request, 'index.html')
 
 # veritabanında şifre ve email doğru olmasına rağmen hata veriyor incelenicek
