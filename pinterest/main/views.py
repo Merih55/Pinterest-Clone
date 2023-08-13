8ced2b6579b5b2b28a98c6a9ecf10b9b3502fb4c
Merih Can Çelenk <merihcancelenk@gmail.com>
2023-08-13 22:10:50 +0300

    login-register
    login ve register işlemleri tamamlandı

--- pinterest/main/views.py
+++ pinterest/main/views.py
@@ -6,36 +6,45 @@ from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def index(request):
-    # register
+
     if request.method == 'POST':
-        kullanici = request.POST['kullanici']
-        email = request.POST['email']
-        sifre1 = request.POST['sifre1']
-        sifre2 = request.POST['sifre2']
-        if sifre1 == sifre2:
-            if User.objects.filter(username = kullanici).exists():
-                messages.error(request, 'Kullanıcı Adı Kullanımda !')
-            elif User.objects.filter(email = email).exists():
-                messages.error(request,'Email Kullanımda !')
-            elif len(sifre1) < 6:
-                messages.error(request, 'Şifre En Az 6 Karakter Olmalıdır !')
-            elif kullanici.lower() in sifre1.lower():
-                messages.error(request, 'Kullanıcı Adı ve Şifre Aynı Olmamalıdır')
+        action = request.POST['action']
+        if action == 'Kayıt Ol':
+            kullanici = request.POST['kullanici']
+            email = request.POST['email']
+            sifre1 = request.POST['sifre1']
+            sifre2 = request.POST['sifre2']
+            if sifre1 == sifre2:
+                if User.objects.filter(username = kullanici).exists():
+                    messages.error(request, 'Kullanıcı Adı Kullanımda !')
+                elif User.objects.filter(email = email).exists():
+                    messages.error(request,'Email Kullanımda !')
+                elif len(sifre1) < 6:
+                    messages.error(request, 'Şifre En Az 6 Karakter Olmalıdır !')
+                elif kullanici.lower() in sifre1.lower():
+                    messages.error(request, 'Kullanıcı Adı ve Şifre Aynı Olmamalıdır')
+                else:
+                    user = User.objects.create_user(
+                        username = kullanici,
+                        email = email,
+                        password = sifre1
+                    )
+                    user.save()
+                    messages.success(request,'Kayıt tamamlandı. Giriş Yapabilirsiniz.')
+                        
             else:
-                user = User.objects.create_user(
-                    username = kullanici,
-                    email = email,
-                    password = sifre1
-                )
-                user.save()
-                messages.success(request,'Kayıt tamamlandı. Giriş Yapabilirsiniz.')
-                    
-        else:
-            messages.error(request,'Şifreler Eşleşmiyor !')
-# Kullanıcı Girişi Fonksiyonu   
+                messages.error(request,'Şifreler Eşleşmiyor !')
+        elif action == 'Giriş Yap':
+            kullanici = request.POST['kullaniciAdi']
+            sifre1 = request.POST['parola']
 
+            user = authenticate(request, username = kullanici, password = sifre1)
 
-            
+            if user is not None:
+                login(request, user)
+                messages.success(request,'Giriş Yapıldı')
+                return redirect('deneme')
+            else:
+                messages.error(request,'Kullanıcı Adı veya Şifre Hatalı')
+                return redirect('index')
     return render (request, 'index.html')
-
-# veritabanında şifre ve email doğru olmasına rağmen hata veriyor incelenicek
--- pinterest/pinterest/urls.py
+++ pinterest/pinterest/urls.py
@@ -15,13 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
 from main.views import *
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',index,name='index'),
+    
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
